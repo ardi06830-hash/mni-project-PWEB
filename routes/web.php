43217595d9fefd515316c46ce1847fe40c187b71
ardi06830-hash <ardi06830@gmail.com>
2026-09-19 19:45:47 +0700

    edit web.php

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\ProfilController;
 
+Route::get('/', [ProfilController::class, 'index']);
 Route::get('/profil', [ProfilController::class, 'index']);
